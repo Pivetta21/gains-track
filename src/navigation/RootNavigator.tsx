82646d3291ba522bd360f800ce82e1db83d64ad8
Lucas Pivetta <lucaspivetta21@gmail.com>
2022-06-09 22:41:12 -0300

    style: format navigation files

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,23 +1,24 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import LogIn from '../screens/LogIn'
-import SignUp from '../screens/SignUp'
-import Welcome from '../screens/Welcome'
-import HomeNavigator from './HomeNavigator'
-import type { RootStackParamList } from './types'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LogIn from '../screens/LogIn';
+import SignUp from '../screens/SignUp';
+import Welcome from '../screens/Welcome';
+import HomeNavigator from './HomeNavigator';
+import type { RootStackParamList } from './types';
 
-const Stack = createNativeStackNavigator<RootStackParamList>()
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
-    return <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name='Welcome' component={Welcome} />
-            <Stack.Screen name='SignUp' component={SignUp} />
-            <Stack.Screen name='LogIn' component={LogIn} />
-            <Stack.Screen name='Home' component={HomeNavigator} />
-        </Stack.Navigator>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="LogIn" component={LogIn} />
+        <Stack.Screen name="Home" component={HomeNavigator} />
+      </Stack.Navigator>
     </NavigationContainer>
-}
+  );
+};
 
-export { RootNavigator as default }
-
+export { RootNavigator as default };
--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,31 +1,34 @@
-import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
-import type { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native'
-import type { NativeStackScreenProps } from '@react-navigation/native-stack'
+import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+import type {
+  CompositeScreenProps,
+  NavigatorScreenParams,
+} from '@react-navigation/native';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 export type RootStackParamList = {
-    Welcome: undefined;
-    LogIn: undefined;
-    SignUp: undefined;
-    Home: NavigatorScreenParams<HomeTabParamList>;
-}
+  Welcome: undefined;
+  LogIn: undefined;
+  SignUp: undefined;
+  Home: NavigatorScreenParams<HomeTabParamList>;
+};
 
 export type RootStackScreenProps<T extends keyof RootStackParamList> =
-    NativeStackScreenProps<RootStackParamList, T>
+  NativeStackScreenProps<RootStackParamList, T>;
 
 export type HomeTabParamList = {
-    Exercises: undefined;
-    Routine: undefined;
-    Settings: undefined;
-}
+  Exercises: undefined;
+  Routine: undefined;
+  Settings: undefined;
+};
 
 export type HomeTabScreenProps<T extends keyof HomeTabParamList> =
-    CompositeScreenProps<
-        BottomTabScreenProps<HomeTabParamList, T>,
-        RootStackScreenProps<keyof RootStackParamList>
-    >
+  CompositeScreenProps<
+    BottomTabScreenProps<HomeTabParamList, T>,
+    RootStackScreenProps<keyof RootStackParamList>
+  >;
 
 declare global {
-    namespace ReactNavigation {
-        interface RootParamList extends RootStackParamList { }
-    }
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
 }
