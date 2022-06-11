9814ca5e1b6be906b27230b86037da8da1d8b397
Lucas Pivetta <lucaspivetta21@gmail.com>
2022-06-10 22:32:52 -0300

    feat: add and load Montserrat font family

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar'
-import { SafeAreaView } from 'react-native'
-import RootNavigator from './src/navigation/RootNavigator'
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native';
+import useAppLoad from './src/hooks/useAppLoad';
+import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
+  const [appIsReady, onLayoutRootView] = useAppLoad();
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <StatusBar style='auto' />
+    <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <StatusBar style="auto" />
       <RootNavigator />
     </SafeAreaView>
   );
---
+++ src/hooks/useAppLoad.tsx
@@ -0,0 +1,44 @@
+import {
+  Montserrat_300Light,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_700Bold,
+  Montserrat_800ExtraBold,
+} from '@expo-google-fonts/montserrat';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+import { LayoutChangeEvent } from 'react-native';
+
+export default function useAppLoad(): [
+  boolean,
+  (event: LayoutChangeEvent) => void,
+] {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+
+      await Font.loadAsync({
+        'Montserrat-Light': Montserrat_300Light,
+        'Montserrat-Regular': Montserrat_400Regular,
+        'Montserrat-Medium': Montserrat_500Medium,
+        'Montserrat-Bold': Montserrat_700Bold,
+        'Montserrat-ExtraBold': Montserrat_800ExtraBold,
+      });
+
+      setAppIsReady(true);
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  return [appIsReady, onLayoutRootView];
+}
