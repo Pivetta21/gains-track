1919a5b122d9b83abea2d13a2995a922e1742a46
Lucas Pivetta <lucaspivetta21@gmail.com>
2022-07-04 21:30:46 -0300

    feat: create LogIn and SignUp screens

--- src/screens/LogIn/index.tsx
+++ src/screens/LogIn/index.tsx
@@ -1,13 +1,58 @@
-import { Button, Text, View } from 'react-native'
-import type { RootStackScreenProps } from '../../navigation/types'
+import Button from '../../components/Button';
+import type { RootStackScreenProps } from '../../navigation/types';
+import { LayoutContainer } from '../../styled/layout';
+import { z } from 'zod';
+import { useForm } from '../../hooks/useForm';
+import Input from '../../components/Input';
+import { useRef } from 'react';
+import { TextInput } from 'react-native';
 
-type Props = RootStackScreenProps<'LogIn'>
+const LogInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
 
-const LogIn = ({ navigation }: Props) => {
-    return <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Log In</Text>
-        <Button title='Home Screen' onPress={() => navigation.navigate('Home')} />
-    </View>
+type LogInType = z.infer<typeof LogInSchema>;
+
+type Props = RootStackScreenProps<'LogIn'>;
+
+export default function LogIn({ navigation }: Props) {
+  const { form, handleFormChange, canSubmit } = useForm(
+    {} as LogInType,
+    LogInSchema,
+  );
+
+  const passwordInputRef = useRef<TextInput>(null);
+
+  function onLoginPress() {
+    if (canSubmit()) navigation.replace('Home', { screen: 'Routine' });
+  }
+
+  return (
+    <LayoutContainer>
+      <Input
+        focusRef={passwordInputRef}
+        value={form.email}
+        onValueChange={(text) => handleFormChange(text, 'email')}
+        placeholder="E-mail"
+        autoCapitalize="none"
+        keyboardType="email-address"
+        leadingIcon="email-outline"
+        autoFocus
+      />
+
+      <Input
+        ref={passwordInputRef}
+        value={form.password}
+        onValueChange={(text) => handleFormChange(text, 'password')}
+        placeholder="Password"
+        leadingIcon="lock-outline"
+        autoCapitalize="none"
+        secureTextEntry={true}
+        trailingIcon="show-hide"
+      />
+
+      <Button title="Log In" onPress={onLoginPress} />
+    </LayoutContainer>
+  );
 }
-
-export { LogIn as default }
--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,14 +1,73 @@
-import { Button, Text, View } from 'react-native'
-import { RootStackScreenProps } from '../../navigation/types'
+import { z } from 'zod';
+import { useRef } from 'react';
+import { ScrollView, TextInput } from 'react-native';
+import { LayoutContainer } from '../../styled/layout';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { useForm } from '../../hooks/useForm';
+import { RootStackScreenProps } from '../../navigation/types';
 
-type Props = RootStackScreenProps<'SignUp'>
+const SignUpSchema = z.object({
+  username: z.string().min(4).max(25),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
 
-const SignUp = ({ navigation }: Props) => {
-  return <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Sign Up</Text>
-    <Button title='Home Screen' onPress={() => navigation.navigate('Home')}/>
-  </View>
+type SignUpType = z.infer<typeof SignUpSchema>;
+
+type Props = RootStackScreenProps<'SignUp'>;
+
+export default function SignUp({ navigation }: Props) {
+  const { form, handleFormChange, canSubmit } = useForm(
+    {} as SignUpType,
+    SignUpSchema,
+  );
+
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
+  function onSignUpPress() {
+    if (canSubmit()) navigation.replace('Home', { screen: 'Routine' });
+  }
+
+  return (
+    <ScrollView overScrollMode="never" keyboardShouldPersistTaps="handled">
+      <LayoutContainer>
+        <Input
+          focusRef={emailInputRef}
+          value={form.username}
+          onValueChange={(text) => handleFormChange(text, 'username')}
+          placeholder="Username"
+          leadingIcon="account-outline"
+          autoCapitalize="words"
+          trailingIcon="clear"
+          autoFocus
+        />
+
+        <Input
+          ref={emailInputRef}
+          focusRef={passwordInputRef}
+          value={form.email}
+          onValueChange={(text) => handleFormChange(text, 'email')}
+          placeholder="E-mail"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          leadingIcon="email-outline"
+        />
+
+        <Input
+          ref={passwordInputRef}
+          value={form.password}
+          onValueChange={(text) => handleFormChange(text, 'password')}
+          placeholder="Password"
+          leadingIcon="lock-outline"
+          autoCapitalize="none"
+          secureTextEntry={true}
+          trailingIcon="show-hide"
+        />
+
+        <Button title="Sign Up" onPress={onSignUpPress} />
+      </LayoutContainer>
+    </ScrollView>
+  );
 }
-
-export { SignUp as default }
-
